6ea208dc9dcec0db6d77de019fed12d3eb8fa1d4
Shadow Panther <panther.ru@gmail.com>
2014-11-03 04:21:51 +0300

    no message

--- BR-Control/BR-Control.lua
+++ BR-Control/BR-Control.lua
@@ -9,7 +9,7 @@ local unicode = require("unicode")
 local sides = require("sides")
 local colors=require("colors")
 
-local gpu=component.gpu
+local gpu = component.gpu
 local br = component.br_reactor
 local rs = component.redstone
 
@@ -25,16 +25,23 @@ term.setCursorBlink(false)
 
 
 -------------------------------------------------------------------------------
+
+
+br.getEnergyPercent = function ()
+  return math.floor(1000 * br.getEnergyStored() / maxEnergy) / 10
+end
+
+
 function getKey()
   return (select(4, event.pull("key_down")))
 end
 
-local function printXY(row, col, s, ...)
+local function printXY(row, col, s)
   gotoXY(col, row)
-  print(s:format(...))
+  print(s)
 end
 
-local function printfXY(row, col, s, ...)
+local function printFXY(row, col, s, ...)
   gotoXY(col, row)
   print(s:format(...))
 end
@@ -73,99 +80,103 @@ local controlKeyCombos = {
 }
 
 
-local function onKeyDown(opt)
-  if opt == keyboard.keys.left then
-    br.setActive(false)
-  elseif opt == keyboard.keys.right then
-    br.setActive(true)
-  elseif opt == keyboard.keys.up then
-    if (rodLevel > 0) then
-      rodLevel = rodLevel - 10
-      br.setAllControlRodLevels(rodLevel)
-    end
-  elseif opt == keyboard.keys.down then
-    if (rodLevel < 100) then
-      rodLevel = rodLevel + 10
-      br.setAllControlRodLevels(rodLevel)
-    end
-  elseif opt == keyboard.keys.pageDown then
-    br.doEjectWaste()
-  elseif opt == keyboard.keys.q then
+local function onKeyDown(key)
+  -- if key == keyboard.keys.left then
+  --   br.setActive(false)
+  -- elseif key == keyboard.keys.right then
+  --   br.setActive(true)
+  -- elseif key == keyboard.keys.up then
+  --   if (rodLevel > 0) then
+  --     rodLevel = rodLevel - 10
+  --     br.setAllControlRodLevels(rodLevel)
+  --   end
+  -- elseif key == keyboard.keys.down then
+  --   if (rodLevel < 100) then
+  --     rodLevel = rodLevel + 10
+  --     br.setAllControlRodLevels(rodLevel)
+  --   end
+  -- elseif key == keyboard.keys.pageDown then
+  --   br.doEjectWaste()
+  if key == keyboard.keys.q then
     running = false
   end
 end
 
+
+function ft ()
+  local d = os.day()
+  local t = os.time()
+  local h = math.floor(t)
+  local m = math.floor((t-h)*60)
+  return string.format("Day %d, %02d:%02d",d,h,m)
+end
+
+
+function log (msg)
+  local stamp=ft()
+  print (stamp..": "..msg)
+end
+
+
+function tableWidth(t)
+  local width=0
+  for _,v in pairs(t) do
+    if #v>width then width=#v end
+  end
+  return width
+end
+
+
+function ljust(s,w)
+  local pad=w-#s
+  return s .. string.rep(" ",pad)
+end
+
+
+function rjust(s,w)
+  local pad=w-#s
+  return string.rep(" ",pad) .. s
+end
+
+
 -------------------------------------------------------------------------------
 
 
-while running do
-  local e=r.getEnergyStored()
-  local p=math.floor(100*e/emax)
-  local a=p<100
-  local elt=r.getEnergyProducedLastTick()
-  display()
-  r.setAllControlRodLevels(p)
-  r.setActive(a)
-
-  
-  local reactorTemp = br.getCasingTemperature()
-  printXY(1,  1, "    Item     Name:          Level:           Temperature:          Fuel:          Max Fuel          Waste:", "")
-  printXY(4,  1, "    1:       %s           %03d              %04d                  %06d         %06d            %04d",br.getControlRodName(0), br.getControlRodLevel(0), br.getFuelTemperature(0), br.getFuelAmount(0), br.getFuelAmountMax(0), br.getWasteAmount(0))
-  printXY(6,  1, "    2:       %s           %03d              %04d                  %06d         %06d            %04d",br.getControlRodName(5), br.getControlRodLevel(5), br.getFuelTemperature(5), br.getFuelAmount(5), br.getFuelAmountMax(5), br.getWasteAmount(5))
-  printXY(8,  1, "    3:       %s           %03d              %04d                  %06d         %06d            %04d",br.getControlRodName(3), br.getControlRodLevel(3), br.getFuelTemperature(3), br.getFuelAmount(3), br.getFuelAmountMax(3), br.getWasteAmount(3))
-  printXY(10, 1, "    4:       %s           %03d              %04d                  %06d         %06d            %04d",br.getControlRodName(6), br.getControlRodLevel(6), br.getFuelTemperature(6), br.getFuelAmount(6), br.getFuelAmountMax(6), br.getWasteAmount(6))
-  printXY(12, 1, "    5:       %s           %03d              %04d                  %06d         %06d            %04d",br.getControlRodName(4), br.getControlRodLevel(4), br.getFuelTemperature(4), br.getFuelAmount(4), br.getFuelAmountMax(4), br.getWasteAmount(4))
-  printXY(14, 1, "    6:       %s           %03d              %04d                  %06d         %06d            %04d",br.getControlRodName(2), br.getControlRodLevel(2), br.getFuelTemperature(2), br.getFuelAmount(2), br.getFuelAmountMax(2), br.getWasteAmount(2))
-  printXY(16, 1, "    7:       %s           %03d              %04d                  %06d         %06d            %04d",br.getControlRodName(7), br.getControlRodLevel(7), br.getFuelTemperature(7), br.getFuelAmount(7), br.getFuelAmountMax(7), br.getWasteAmount(7))
-  printXY(18, 1, "    8:       %s           %03d              %04d                  %06d         %06d            %04d",br.getControlRodName(1), br.getControlRodLevel(1), br.getFuelTemperature(1), br.getFuelAmount(1), br.getFuelAmountMax(1), br.getWasteAmount(1))
-  printXY(20, 1, "    9:       %s           %03d              %04d                  %06d         %06d            %04d",br.getControlRodName(8), br.getControlRodLevel(8), br.getFuelTemperature(8), br.getFuelAmount(8), br.getFuelAmountMax(8), br.getWasteAmount(8))
-  printXY(22, 1, "   10:       Reactor        %03d              %04d                  %06d         %06d            %04d", br.getControlRodLevel(0), br.getCasingTemperature(), br.getFuelAmount(), br.getFuelAmountMax(), br.getWasteAmount())
-  printXY(25, 1, "   11:       Energy Stored           %06.2f", br.getEnergyStored())
-  printXY(26, 1, "   12:       Energy Last Tick           %06.2f", br.getEnergyProducedLastTick())
-  
-  centerF(40, "Data updates every second     Tick Count:  %2d", tickCnt)
-  centerF(41, "Current up time:             %2d hours %2d min", hours, mins)
-  center(42, "Left - Turn Reactor Off     Right - Turn Reactor On")
-  center(43, "Up - Pull Rods 10%          Down - Lower Rods 10%  ")
-  center(44, "Page Down - Eject Waste     Q - Quit               ")
-  --center(39, "  ")
-  
-  if (reactorTemp > maxTemp) then
-    term.setCursor(25,30)
-    gpu.setForeground(0xFF0000)
-    gpu.setBackground(0xFFFFFF)
-    warning(30, "                                         ")
-    warning(31, "     WARNING     WARNING     WARNING     ")
-    warning(32, "                                         ")
-    warning(33, "       Reactor Temperature Too High       ")
-    warning(34, "                                         ")
-    gpu.setForeground(0xFFFFFF)
-    gpu.setBackground(0x000000)
-    term.setCursor(1,41)
-  else
-    gpu.setForeground(0xFFFFFF)
-    gpu.setBackground(0x000000)
-    gotoXY(30,1)
-    print()
-    gotoXY(31,1)
-    print()
-    gotoXY(32,1)
-    print()
-    gotoXY(33,1)
-    print()
-    gotoXY(34,1)
-    print()
-    gpu.setForeground(0xFFFFFF)
-    gpu.setBackground(0x000000)
-    term.setCursor(1,47)
+function display()
+  term.clear()
+  printXY(1, 1, "Reactor Status")
+  printXY(2, 1, ft())
+  local funcs={"Connected","Active","NumberOfControlRods","EnergyStored","EnergyPercent","CasingTemperature","FuelTemperature","FuelAmount","WasteAmount","FuelAmountMax","EnergyProducedLastTick","FuelConsumedLastTick"}
+  local units={"","","","RF","%","C","C","mB","mB","mB","RF/t","mB/t"}
+  local values={}
+  for _,v in pairs(funcs) do
+    values[#values+1] = tostring(r["get"..v]())
   end
+  local funcW=tableWidth(funcs)
+  local valW=tableWidth(values)
+  gotoXY(4, 1)
+  for i,v in pairs(funcs) do
+    print(rjust(v,funcW)..": "..rjust(values[i],valW).." "..units[i])
+  end  
+end
+
+
+while running do
+  local currentEnergy = br.getEnergyStored()
+  local currentEnergyPercent = math.floor(100*currentEnergy/maxEnergy)
+  local reactorActive = currentEnergyPercent < 100
+  local reactorEnergyLastTick = br.getEnergyProducedLastTick()
+  display()
+  br.setAllControlRodLevels(currentEnergyPercent)
+  br.setActive(reactorActive)
+
   term.clearLine()
   print() 
   local event, address, arg1, arg2, arg3 = event.pull(1)
   if type(address) == "string" and component.isPrimary(address) then
-  if event == "key_down" then
-    onKeyDown(arg2)
-  end
+    if event == "key_down" then
+      onKeyDown(arg2)
+    end
   end
 end
 
